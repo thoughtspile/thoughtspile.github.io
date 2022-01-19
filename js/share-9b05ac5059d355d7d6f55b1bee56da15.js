d5356f98d83d84d76c3072e7ed37fd6751930e21
Vladimir Klepov <v.klepov@gmail.com>
2022-01-19 13:20:27 +0300

    Site updated: 2022-01-19 13:20:26

--- js/share-9b05ac5059d355d7d6f55b1bee56da15.js
+++
@@ -1,1 +0,0 @@
-!function(){var t=document.getElementById("share-button");navigator.share?t.addEventListener("click",function(t){t.preventDefault(),navigator.share({title:document.title,url:location.href})}):t.style.display="none"}();
